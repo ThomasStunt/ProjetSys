109ff02ffd46cddbe417b86751d1eb0ad7938ea8
Benjamin DUCAUROY <benj62149@gmail.com>
2016-01-26 09:56:09 +0100

    main.c webserver

---
+++ webserver/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if(argc>1 && strcmp(argv[1], "-advice")==0){
+		printf("Don't Panic!\n");
+		return 42;
+	}
+	printf("Need an advice");
+	return 0;
+}
